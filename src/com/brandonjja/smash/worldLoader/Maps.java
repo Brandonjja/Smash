1238c3b1270f1f70be042d0b9d3d32209914c104
Brandon <brandonjja@hotmail.com>
2020-11-20 05:09:13 -0400

    Added support for map list in config

--- src/com/brandonjja/smash/worldLoader/Maps.java
+++ src/com/brandonjja/smash/worldLoader/Maps.java
@@ -1,26 +1,13 @@
 package com.brandonjja.smash.worldLoader;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import com.brandonjja.smash.Smash;
+
 public class Maps {
-	private static List<String> maps = new ArrayList<>();
+	private static List<String> maps = Smash.getInstance().getConfig().getStringList("maps");
 	
 	public static List<String> getMaps() {
-		maps = new ArrayList<>();
-		maps.add("Angry Birds");
-		maps.add("Aquartez");
-		maps.add("Imagination");
-		
-		maps.add("Block Fort");
-		maps.add("Supernova");
-		
-		maps.add("EndStone");
-		maps.add("Catch 'em All");
-		maps.add("Time's Up"); // Clock
-		maps.add("Sandy Dayz");
-		maps.add("Star");
-		maps.add("Snowflake");
 		return maps;
 	}
 }
